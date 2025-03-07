41139c34788fb91c85a3902d9700fea19f0aa298
Ezequiel Alonzo <ezequielalonzo21@gmail.com>
2025-03-06 20:25:30 -0500

    Update

--- index.php
+++ index.php
@@ -1,68 +1,60 @@
 <!DOCTYPE html>
 <html lang="es">
-	<head>
-      <script src="https://challenges.cloudflare.com/turnstile/v0/api.js" async defer></script>
-		<script async src="https://www.googletagmanager.com/gtag/js?id=G-EYRG621ZF3"></script>
-		<script>
-			window.dataLayer = window.dataLayer || [];
-
-			function gtag() {
-				dataLayer.push(arguments);
-			}
-			gtag("js", new Date());
-
-			gtag("config", "G-EYRG621ZF3");
-		</script>
-		<meta charset="UTF-8" />
-		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
-		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-		<title>Ezequiel Alonzo - Desarrollador Web</title>
-		<link rel="icon" href="media/favicon.svg?v=2025" sizes="32x32" />
-		<link rel="icon" href="media/favicon.svg?v=2025" sizes="192x192" />
-		<link rel="apple-touch-icon" href="media/favicon.svg?v=2025" />
-		<link rel="stylesheet" href="css/bundle.min.css?v=2025" />
-
-	</head>
-
-	<body>
-		<header>
-			<div class="ea_dark"><span class="switch"></span> DARK MODE</div>
-			<!-- <div class="ea_lang"><span class="switch"></span> EN</div> -->
-		</header>
-		<main>
-			<img src="media/logo.svg?v=2025" class="ea_logus" />
-			<h1>Autodidacta, Desarrollador web, Músico y catador de arepas profesional</h1>
-
-			<div class="ea_info_wrap">
-				<div class="ea_info">
-					<h4>Desde 2005:</h4>
-					<p>Desarrollando themes desde cero para <em>WordPress</em> / <em>Woocommerce</em></p>
-					<p>
-						Clientes satisfechos en Miami (EEUU), Lima (Perú), Bogotá (Colombia), Ciudad de México (México), Madrid (España), Karava (Finlandia), Londres (UK), New
-						Plymouth (Nueva Zelanda), San Salvador (El Salvador), Santiago (Chile) y Mayagüez (Pto. Rico).
-					</p>
-				</div>
-
-				<iframe
-					src="https://spotify-recently-played-readme.vercel.app/api?user=id9zhabk3cqm8mz161qkor09n&unique=1&count=3&width=400"
-					frameborder="0"
-					style="width: 400px; min-height: 216px"
-				></iframe>
-			</div>
-		</main>
-		<footer>
-			<ul>
-				<li><a href="mailto:ezequielalonzo21@gmail.com">Email</a></li>
-				<li><span>|</span></li>
-				<li><a href="//wa.me/593979150870">WhatsApp</a></li>
-				<li><span>|</span></li>
-				<li><a href="//linkedin.com/in/ezequiel-alonzo/">LinkedIn</a></li>
-				<li><span>|</span></li>
-				<li><a href="//github.com/Ezekiel1349/">GitHub</a></li>
-			</ul>
-		</footer>
-
-		<script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
-		<script src="js/main.js?v=0.1.6"></script>
-	</body>
+<head>
+   <script src="https://challenges.cloudflare.com/turnstile/v0/api.js" async defer></script>
+   <script async src="https://www.googletagmanager.com/gtag/js?id=G-EYRG621ZF3"></script>
+   <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag() {
+         dataLayer.push(arguments);
+      }
+      gtag("js", new Date());
+      gtag("config", "G-EYRG621ZF3");
+   </script>
+   <meta charset="UTF-8" />
+   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+   <title>Ezequiel Alonzo - Desarrollador Web</title>
+   <link rel="icon" href="media/favicon.svg?v=2025" sizes="32x32" />
+   <link rel="icon" href="media/favicon.svg?v=2025" sizes="192x192" />
+   <link rel="apple-touch-icon" href="media/favicon.svg?v=2025" />
+   <link rel="stylesheet" href="css/bundle.min.css?v=2025" />
+</head>
+<body>
+   <header>
+      <div class="ea_dark"><span class="switch"></span> DARK MODE</div>
+      <!-- <div class="ea_lang"><span class="switch"></span> EN</div> -->
+   </header>
+   <main>
+      <img src="media/logo.svg?v=2025" class="ea_logus" />
+      <h1>Autodidacta, Desarrollador web, Músico y catador de arepas profesional</h1>
+      <div class="ea_info_wrap">
+         <div class="ea_info">
+            <h4>Desde 2005:</h4>
+            <p>Especialista en <em>WordPress</em> / <em>Woocommerce</em></p>
+            <p>He dejado huella en proyectos para clientes en más de 10 países, incluyendo EE.UU., España, El Salvador, Ecuador, Chile y México, principalmente como aliado estratégico de agencias. Mi enfoque es simple pero efectivo: construir sitios web que no solo se vean bien, sino que impulsen resultados reales para las empresas.
+            </p>
+            <p>
+               ¿Buscas mejorar tu presencia online? ¡Conectemos!</p>
+         </div>
+         <iframe
+            src="https://spotify-recently-played-readme.vercel.app/api?user=id9zhabk3cqm8mz161qkor09n&unique=1&count=3&width=400"
+            frameborder="0"
+            style="width: 400px; min-height: 216px"></iframe>
+      </div>
+   </main>
+   <footer>
+      <ul>
+         <li><a href="mailto:ezequielalonzo21@gmail.com">Email</a></li>
+         <li><span>|</span></li>
+         <li><a href="//wa.me/593979150870">WhatsApp</a></li>
+         <li><span>|</span></li>
+         <li><a href="//linkedin.com/in/ezequiel-alonzo/">LinkedIn</a></li>
+         <li><span>|</span></li>
+         <li><a href="//github.com/Ezekiel1349/">GitHub</a></li>
+      </ul>
+   </footer>
+   <script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
+   <script src="js/main.js?v=0.1.6"></script>
+</body>
 </html>
---
+++ misc/fima.php
@@ -0,0 +1,12 @@
+<table style=" border: 1px solid #ccc; width: 100%; max-width: 800px; padding: 12px; line-height: 100%;">
+   <tr>
+      <td><img src="https://ezekiel1349.github.io/media/mail_full.svg" style="width: 800px" /></td>
+   </tr>
+   <tr>
+      <td style="padding: 12px 0 0">
+         <p style="color: #324359;margin-top: 0;font-size: 12px;font-family: 'system-ui', sans-serif;">El contenido de este correo electrónico y sus anexos es estrictamente confidencial. Si usted no es el destinatario y ha recibido este mensaje por error, le agradeceríamos que lo notificara de inmediato al remitente y se abstuviera de divulgar, almacenar o copiar su contenido.</p>
+         <p style="color: #919191;margin: 0;font-size: 12px;font-family: 'system-ui', sans-serif;"> The content of this email and its attachments is strictly confidential. If you are not the intended recipient and have received this message in error, please notify the sender immediately and refrain from sharing, storing, or copying its content.
+         </p>
+      </td>
+   </tr>
+</table>
