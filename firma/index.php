f1f05382bb8b877db7aafda9e8a61691779cd265
Ezequiel Alonzo <ezequielalonzo21@gmail.com>
2026-04-09 00:02:39 -0500

    Otros Cambios

--- firma/index.php
+++ firma/index.php
@@ -37,28 +37,26 @@
       </tr>
    </tbody>
 </table>
-
-
-<table cellpadding="10" cellspacing="0" style="border: 0; line-height: 0; width: 865px">
+<table cellpadding="10" cellspacing="0" style="border: 0; line-height: 0; width: 808px">
    <tbody>
       <tr>
          <td style="width: 229px;">
-            <img src="f_logo.png" style="width: 100%;">
+            <img src="https://alonzo.dev/firma/f_logo.png" style="width: 100%;">
          </td>
-         <td style="width: 380px;">
-            <img src="f_text.png" style="max-width: 100%;">
+         <td style="width: 322px;">
+            <img src="https://alonzo.dev/firma/f_text.png" style="max-width: 100%;">
          </td>
          <td>
             <table style="border: 0; line-height: 0">
                <tbody>
                   <tr>
-                     <td><a target="_blank" href="https://wa.me/593979150870" style="display: block;padding: 4px 0"><img src="f_wa.png" style="max-width: 100%;"></a></td>
+                     <td><a target="_blank" href="https://wa.me/593979150870" style="display: block;padding: 4px 0"><img src="https://alonzo.dev/firma/f_wa.png" style="max-width: 100%;"></a></td>
                   </tr>
                   <tr>
-                     <td><a target="_blank" href="https://www.linkedin.com/in/ezequiel-alonzo/" style="display: block;padding: 4px 0"><img src="f_in.png" style="max-width: 100%;"></a></td>
+                     <td><a target="_blank" href="https://www.linkedin.com/in/ezequiel-alonzo/" style="display: block;padding: 4px 0"><img src="https://alonzo.dev/firma/f_in.png" style="max-width: 100%;"></a></td>
                   </tr>
                   <tr>
-                     <td><a target="_blank" href="" style="display: block;padding: 4px 0"><img src="f_web.png" style="max-width: 100%"></a></td>
+                     <td><a target="_blank" href="" style="display: block;padding: 4px 0"><img src="https://alonzo.dev/firma/f_web.png" style="max-width: 100%"></a></td>
                   </tr>
                </tbody>
             </table>
@@ -66,7 +64,7 @@
       </tr>
       <tr>
          <td colspan="3">
-            <img src="f_sep.png" style="width: 100%;">
+            <img src="https://alonzo.dev/firma/f_sep.png" style="width: 100%;">
          </td>
       </tr>
       <tr>
@@ -78,3 +76,23 @@
       </tr>
    </tbody>
 </table>
+
+
+
+
+<div style="display: inline-flex; flex-direction: column; width: fit-content; max-width: 814px; padding: 0 12px; border: 0; line-height: 0;">
+   <div style="display: inline-flex; align-items: center; gap: 24px; margin: 24px 0 18px; border-bottom: 4px solid #ed9347; width: fit-content; padding: 0 0 18px;">
+      <img src="https://alonzo.dev/firma/f_logo.png" style="max-width: 100%;">
+      <img src="https://alonzo.dev/firma/f_text.png" style="max-width: 100%;">
+      <div class="" style="display: flex;flex-direction: column;gap: 4px;margin-top: 8px;">
+         <a target="_blank" href="https://wa.me/593979150870" style="display: block;padding: 4px 0"><img src="https://alonzo.dev/firma/f_wa.png" style="max-width: 100%;"></a>
+         <a target="_blank" href="https://www.linkedin.com/in/ezequiel-alonzo/" style="display: block;padding: 4px 0"><img src="https://alonzo.dev/firma/f_in.png" style="max-width: 100%;"></a>
+         <a target="_blank" href="" style="display: block;padding: 4px 0"><img src="https://alonzo.dev/firma/f_web.png" style="max-width: 100%"></a>
+      </div>
+   </div>
+   <div>
+      <p style="color: #2C2D2A;margin-bottom: 10px;margin-top: 0;font-size: 12px;font-family: 'system-ui', sans-serif;line-height: normal;">El contenido de este correo electrónico y sus anexos es estrictamente confidencial. Si usted no es el destinatario y ha recibido este mensaje por error, le agradeceríamos que lo notificara de inmediato al remitente y se abstuviera de divulgar, almacenar o copiar su contenido.</p>
+      <p style="color: #919191;margin: 0;font-size: 12px;font-family: 'system-ui', sans-serif;line-height: normal;"> The content of this email and its attachments is strictly confidential. If you are not the intended recipient and have received this message in error, please notify the sender immediately and refrain from sharing, storing, or copying its content.
+      </p>
+   </div>
+</div>
